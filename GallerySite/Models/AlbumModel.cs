e37fb1a258375f86784b9bca0ab2b5cab2ac2197
Martin-R-Peterson <martin.peterson.1993@gmail.com>
2022-07-21 16:50:21 +0200

    Add project files.

---
+++ GallerySite/Models/AlbumModel.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GallerySite.Models
+{
+    public class AlbumModel
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Title { get; set; }
+    }
+}
---
+++ GallerySite/Models/ImageModel.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GallerySite.Models
+{
+    public class ImageModel
+    {
+        public int AlbumId { get; set; }
+        [Key]
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Url { get; set; }
+        public string ThumbnailUrl { get; set; }
+
+    }
+}
